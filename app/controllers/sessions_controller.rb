8231a115318b2b7bda42b295ce8d3abed113617a
rahulsharma-kellogg <rahul.sharma2@kellogg.northwestern.edu>
2026-03-17 20:30:53 +0000

    User Authentication Work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome!"
+        redirect_to "/"
+      else
+        flash["notice"] = "Some information is incorrect. Please try again."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Some information is incorrect. Please try again."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "You are now logged out."
+    redirect_to "/login"
   end
 end
   
